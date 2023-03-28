7ea616554d8950434a8bde5fa556204e75d888c6
Meritei-56 <shadrackmeritei@gmail.com>
2023-03-28 04:30:44 -0700

    Ammended

--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+int _putchar(char characters);
 int _printf(const char *format, ...);
 int printf_string(va_list args, int printed_characters);
 
