d4d202d997ec451e0dbc4dc8bd9bac11875a24a8
Meritei-56 <shadrackmeritei@gmail.com>
2023-03-28 01:20:00 -0700

    A function that produces output according to a format

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _puts(char *str);
+
+
+
+#endif
---
+++ printf.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ *
+ *
+ */
+
+int _puts(char *str)
+{
+	int str_ptn;
+
+	for (str_ptn = 0; str[str_ptn] != '\0'; str_ptn++)
+	{
+		_putchar(str[str_ptn]);
+	}
+
+	return (str_ptn);
+}
+/**
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list parameters;
+	int format_ptn, printed_characters;
+
+
+	va_start(parameters, format);
+/**
+ * create code
+ *
+ */
+
+	format_ptn = 0;
+	printed_characters = 0;
+
+	while (format[format_ptn])
+	{
+		if (format[format_ptn] == '%')
+		{
+			if (format[format_ptn + 1] == 'c')
+			{
+				_putchar(va_arg(parameters, int));
+				printed_characters++;
+				format_ptn += 2;
+			}
+			else if (format[format_ptn + 1] == 's')
+			{
+				printed_characters = _puts(va_arg(parameters, char *));
+				format_ptn += 2;
+			}
+			else
+			{
+				_putchar(format[format_ptn]);
+				printed_characters++;
+				format_ptn++;
+			}
+		}
+		else
+		{
+			_putchar(format[format_ptn]);
+			printed_characters++;
+			format_ptn++;
+		}
+	}
+
+	va_end(parameters);
+
+	return (printed_characters);
+}
