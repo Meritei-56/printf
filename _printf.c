72033487ebef71fbe32f62fe52612762de652a84
Meritei-56 <shadrackmeritei@gmail.com>
2023-03-28 13:31:36 -0700

    A function that produces output - code ammended

--- _printf.c
+++ _printf.c
@@ -1,71 +1,75 @@
 #include "main.h"
+#include <stdarg.h>
 
-void print_buffer(char buffer[], int *buff_count);
+int _puts(char *str);
 
 /**
- * _printf - function definition
- * Description: prints
- * @format: a character string
- * Return: Printed characters
+ * _printf - function
+ * Description: produces output according to a format
+ * @format: character string
+ *
+ * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-	int n, count = 0, printed_characters = 0;
-	int width, flags, size, precision, buff_count = 0;
-	char buffer[BUFF_SIZE];
-	va_list arglist;
+	va_list parameters;
+	int format_ptn, printed_characters;
 
-	if (format == NULL)
-		return (-1);
+	va_start(parameters, format);
 
-	va_start(arglist, format);
+	format_ptn = 0;
+	printed_characters = 0;
 
-	for (n = 0; format && format[n] != '\0'; n++)
+	while (format[format_ptn])
 	{
-		if (format[n] != '%')
+		if (format[format_ptn] == '%')
 		{
-			buffer[buff_count++] = format[n];
-			if (buff_count == BUFF_SIZE)
-				print_buffer(buffer, &buff_count);
-
-			printed_characters++;
+			if (format[format_ptn + 1] == 'c')
+			{
+				_putchar(va_arg(parameters, int));
+				printed_characters++;
+				format_ptn += 2;
+			}
+			else if (format[format_ptn + 1] == 's')
+			{
+				printed_characters = _puts(va_arg(parameters, char *));
+				format_ptn += 2;
+			}
+			else
+			{
+				_putchar(format[format_ptn]);
+				printed_characters++;
+				format_ptn++;
+			}
 		}
 		else
 		{
-
-			print_buffer(buffer, &buff_count);
-			width = get_width(format, &n, arglist);
-			flags = get_flags(format, &n);
-			precision = get_precision(format, &n, arglist);
-			size = get_size(format, &n);
-			n++;
-			count = handle_print(format, &n, arglist, buffer,
-				flags, width, precision, size);
-
-			if (count == -1)
-				return (-1);
-			printed_characters += count;
+			_putchar(format[format_ptn]);
+			printed_characters++;
+			format_ptn++;
 		}
 	}
-
-	print_buffer(buffer, &buff_count);
-
-	va_end(arglist);
+	va_end(parameters);
 
 	return (printed_characters);
 }
-
 /**
- * print_buffer - function
- * Description: prints contents of a buffer
- * @buffer: array
- * @buff_count: index to add next
+ * _puts - function
+ * Description: determines string length
+ * @str: string
+ *
+ * Return: string length
  */
-void print_buffer(char buffer[], int *buff_count)
+int _puts(char *str)
 {
-	if (*buff_count > 0)
-		write(1, &buffer[0], *buff_count);
+	int str_ptn;
 
-	*buff_count = 0;
+	for (str_ptn = 0; str[str_ptn] != '\0'; str_ptn++)
+	{
+	_putchar(str[str_ptn]);
+	}
+
+	return (str_ptn);
 }
 
